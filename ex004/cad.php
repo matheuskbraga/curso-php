82e72246a78a3e7eb436980db0bf73e82d992a2f
Matheus Braga <matheus.braga@maristabrasil.org>
2026-06-29 17:29:25 -0300

    Conclusão do exercício 4 sobre Formulários no PHP

---
+++ ex004/cad.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Resultado do processamento</h1>
+    </header>
+
+    <main>
+        <?php 
+        
+        $nome = $_GET["nome"] ?? "(Nome não informado)";
+        $sobrenome = $_GET["sobrenome"] ?? "(Sobrenome não informado)";
+        $idade = $_GET["idade"] ?? "(Idade não informado)";
+        $cidade = $_GET["cidade"] ?? "(Cidade não informado)";
+        
+        echo "Olá ", $nome, " ", $sobrenome, " prazer em te conhecer!";
+        echo "<br>";
+        echo "<br>";
+        echo "Você tem ", $idade, " anos de idade e você é de ", $cidade, ".";
+
+        ?>
+
+        <p><a href="javascript:history.go(-1)">Voltar para a página anterior</a></p>
+    </main>
+</body>
+</html>
