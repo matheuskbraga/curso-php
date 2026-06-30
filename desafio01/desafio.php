5f59551bc9f709980bdf8eb623540fb051d1dbe5
Matheus Braga <matheus.braga@maristabrasil.org>
2026-06-30 12:22:14 -0300

    Organizando o style.css.

--- desafio01/desafio.php
+++ desafio01/desafio.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desafio 1</title>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="../style.css">
 </head>
 <body>
 
@@ -15,7 +15,7 @@
     <main>
         <?php 
     
-    $numero = $_GET["numero"];
+    $numero = $_GET["numero"] ?? "Valor não informado!";
 
     $sucessor = $numero + 1;
     $antecessor = $numero - 1;
