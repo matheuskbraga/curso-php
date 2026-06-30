d24f62c3ad97ca7b95ec23f7da4361f7cdaad412
Matheus Braga <matheus.braga@maristabrasil.org>
2026-06-30 11:51:26 -0300

    Adicionando concatenação de string ao exemplo.

--- ex004/cad.php
+++ ex004/cad.php
@@ -18,12 +18,16 @@
         $sobrenome = $_GET["sobrenome"] ?? "(Sobrenome não informado)";
         $idade = $_GET["idade"] ?? "(Idade não informado)";
         $cidade = $_GET["cidade"] ?? "(Cidade não informado)";
+        $data = "O ano é " . "2026.";
         
         echo "Olá ", $nome, " ", $sobrenome, " prazer em te conhecer!";
         echo "<br>";
         echo "<br>";
         echo "Você tem ", $idade, " anos de idade e você é de ", $cidade, ".";
-
+        echo "<br>";
+        echo "<br>";
+        echo $data;
+        
         ?>
 
         <p><a href="javascript:history.go(-1)">Voltar para a página anterior</a></p>
